e7fe8e77b7c656fc9e87a96f2a7a0ab5e40f5be5
jendoebelin <jennifer.doebelin@gmail.com>
2023-07-29 13:42:09 -0500

    code review

--- salary.py
+++ salary.py
@@ -1,17 +1,39 @@
-def salary_calculator (basesalary):
-	numofweeks = 52
-	numhours = 40
-	return (basesalary / numofweeks) / (numhours)
-	
-	
-def hourly_calculator (hourlywage):
-	numofweeks = 52
-	numhours = 40
-	return (hourlywage * numhours) * numofweeks
-	
-	
-	
-print (hourly_calculator(24.04))
-print (salary_calculator(50000))
-print (f'The hourly rate for a person making $50,000 a year would be ${round(salary_calculator(50000), 2)}')
-print (f'The annual salary for a person making $24.04 per an hour is ${int(round(hourly_calculator(24.04)))}')
+# Declare constants
+NUM_OF_WEEKS = 52
+NUM_HOURS = 40
+
+def salary_calculator(basesalary):
+    """Calculate hourly rate from base salary.
+
+    Args:
+    basesalary: Base annual salary.
+
+    Returns:
+    Hourly wage equivalent of the base salary.
+    """
+    return round(basesalary / NUM_OF_WEEKS / NUM_HOURS, 2)
+
+
+def hourly_calculator(hourlywage):
+    """Calculate annual salary from hourly wage.
+
+    Args:
+    hourlywage: Hourly wage.
+
+    Returns:
+    Base salary equivalent of the hourly wage.
+    """
+    return round(hourlywage * NUM_HOURS * NUM_OF_WEEKS, 2)
+
+
+def main():
+    basesalary = float(input("What is your base salary?"))
+    hourlywage = float(input("What is your hourly wage to the nearest cent?"))
+
+    print(f'The hourly rate for a person making {basesalary} a year would be ${salary_calculator(basesalary)}')
+    print(f'The annual salary for a person making {hourlywage} per an hour is ${hourly_calculator(hourlywage)}')
+
+
+# Start the program
+if __name__ == "__main__":
+    main()
